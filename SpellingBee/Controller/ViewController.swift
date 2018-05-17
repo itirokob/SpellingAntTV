5f3cdf55bfa20be654c9ecdeabd701711b6d9c53
Bianca Itiroko <biancaitiroko@10-40-40-37.0101.ecps.br.iosda.org>
2018-05-17 17:28:09 -0300

    Consertamos erros de PDB, hold to speak e mudancas no storyboard

--- SpellingBee/Controller/ViewController.swift
+++ SpellingBee/Controller/ViewController.swift
@@ -14,47 +14,106 @@ class ViewController: UIViewController {
     
     @IBOutlet var letterImages: [UIImageView]!
     
+    var lettersArray:[Character] = []
+    
+    var currentWord = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         multipeerService.delegate = self
+        
+        currentWord = "ENCODED"//Rand
+    }
+    
+    @IBAction func dictateWordButton(_ sender: Any) {
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        DispatchQueue.main.async {
+            for imageView in self.letterImages{
+                imageView.image = nil
+            }
+        }
     }
 }
 
 extension ViewController: MultipeerDelegate {
+    
+    /// The receivedText func is the common func with the other multipeer machine connected, it receives the message
+    ///
+    /// - Parameter text: received message
     func receivedText(text:String){
-        
         print("Text sent: \(text)")
-        
+    
         if text == "HINT_BUTTON"{
             print("HINT_BUTTON")
             //Play hint
         } else if text == "REPEAT_BUTTON"{
             print("REPEAT_BUTTON")
             //Play repeat
+        } else if text == "END_OF_SPEECH" {
+            //Trata a palavra pra ver se está certo
+            
+            let inputWord = self.lettersArrayToString(lettersArray: self.lettersArray)
+            
+            if inputWord == currentWord {
+                popAlert(title: "YAY", message: "Right answer!")
+                print("Right answer")
+            } else {
+                popAlert(title: "Oh no!", message: "Wrong answer!")
+                print("Wrong answer")
+            }
         } else {
             DispatchQueue.main.async {
                 for imageView in self.letterImages{
                     imageView.image = nil
                 }
+                self.lettersArray = self.checkUserInput.treatUserInput(input: text.wordList, rightWord: self.currentWord)
                 
-                let lettersArray = self.checkUserInput.treatUserInput(input: text.wordList)
-                
-                self.updateSpelledLetters(lettersArray: lettersArray)
+                self.updateSpelledLetters(lettersArray: self.lettersArray)
             }
         }
     }
     
-    func updateSpelledLetters(lettersArray:[String]){
+    func lettersArrayToString(lettersArray:[Character]) -> String{
+        let characterArray = lettersArray.flatMap { $0 } // also works
+        let string = String(characterArray)
+        return string
+    }
+    
+    func popAlert(title:String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: NSLocalizedString("Try again", comment: "Try again"), style: .default, handler: { _ in
+            NSLog("The \"try again\" alert occured.")
+        }))
+        
+        alert.addAction(UIAlertAction(title: NSLocalizedString("Next", comment: "Next"), style: .default, handler: { _ in
+            NSLog("The \"next\" alert occured.")
+            self.currentWord = "APPLE" //CHOOSE A NEW RAND WORD
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
+
+    }
+    
+    /// The updateSpelledLetters func transforms the letters received with multipeer in the images displayed
+    ///
+    /// - Parameter lettersArray: letters to be shown
+    func updateSpelledLetters(lettersArray:[Character]){
         for i in 0..<lettersArray.count{
             for imageView in letterImages{
                 if imageView.tag == i{
-                    imageView.image = UIImage(named: lettersArray[i])
+                    imageView.image = UIImage(named: String(lettersArray[i]))
                 }
             }
         }
     }
 }
 
+
+// MARK: - Transforms a string to a list of words
 extension String {
     var wordList: [String] {
         return components(separatedBy: .punctuationCharacters)
--- SpellingBee/Model/Services/TreatInputService.swift
+++ SpellingBee/Model/Services/TreatInputService.swift
@@ -15,8 +15,8 @@ class TreatInputService:NSObject {
     ///
     /// - Parameter input: array with the words said by the user in text format (can contain intepretation mistakes from Speech framework)
     /// - Returns: array with the letters said by the user
-    func treatUserInput(input:[String]) -> [String] {
-        var finalArray:[String] = []
+    func treatUserInput(input:[String], rightWord:String) -> [Character] {
+        var finalArray:[Character] = []
         
         //Quando a pessoa fala a palavra no começo, fazer um tratamento pra ignorar isso
         
@@ -24,21 +24,62 @@ class TreatInputService:NSObject {
         //Se não, adiciona no array final
         for element in input {
             if element.count == 1 {
-                finalArray.append(element.uppercased())
+                finalArray.append(Character(element.uppercased()))
             } else {
                 let newArray = findLetterMatch(inputWord: element.uppercased())
                 for i in newArray {
-                    finalArray.append(String(i).uppercased())
+                    finalArray.append(Character(String(i).uppercased()))
                 }
             }
         }
         
+        finalArray = fixSiriPBDMistakes(finalArray: finalArray, rightWord: turnStringArrayToCharacterArray(array: Array(rightWord)))
+        
         return finalArray
     }
     
-    // D E F G H I J K L M N P Q T V W X Y Z
-    func findLetterMatch(inputWord:String) -> Array<String> {
-        var returnArray:[String] = []
+    func fixSiriPBDMistakes(finalArray:[Character], rightWord:[Character]) -> [Character]{
+        var fixedArray:[Character] = finalArray
+        
+        if finalArray.count <= rightWord.count {
+            for i in 0..<finalArray.count{
+                if finalArray[i] != rightWord[i]{
+                    if ((finalArray[i] == "B") && (rightWord[i] == "P")){
+                        fixedArray[i] = "P"
+                    } else if ((finalArray[i] == "P") && (rightWord[i] == "B")){
+                        fixedArray[i] = "B"
+                    } else if ((finalArray[i] == "D") && (rightWord[i] == "B")){
+                        fixedArray[i] = "B"
+                    } else if ((finalArray[i] == "B") && (rightWord[i] == "D")){
+                        fixedArray[i] = "D"
+                    } else if ((finalArray[i] == "A") && (rightWord[i] == "E")){
+                        fixedArray[i] = "E"
+                    } else if ((finalArray[i] == "E") && (rightWord[i] == "A")){
+                        fixedArray[i] = "A"
+                    }
+                }
+            }
+        }
+        
+        return fixedArray
+    }
+    
+    func turnStringArrayToCharacterArray(array:Array<Character>) -> [Character]{
+        var newArray:[Character] = []
+
+        for letter in array {
+            newArray.append(letter)
+        }
+        
+        return newArray
+    }
+    
+    /// The findLetterMatch func treats possible mistakes from Speech voice recognition
+    ///
+    /// - Parameter inputWord: word received from Speech voice recognition
+    /// - Returns: returns a array of letters
+    func findLetterMatch(inputWord:String) -> Array<Character> {
+        var returnArray:[Character] = []
         
         if (inputWord == "HEY") {
             returnArray.append("A")
@@ -54,7 +95,7 @@ class TreatInputService:NSObject {
             returnArray.append("Q")
         } else if(inputWord == "0") || (inputWord == "OH"){
             returnArray.append("O")
-        } else if (inputWord == "AM") {
+        } else if (inputWord == "AM") || (inputWord == "MAN") {
             returnArray.append("M")
         } else if(inputWord == "AND") {
             returnArray.append("N")
@@ -65,7 +106,7 @@ class TreatInputService:NSObject {
                 if letter == "0" {
                     returnArray.append("O")
                 } else {
-                    returnArray.append(String(letter))
+                    returnArray.append(letter)
                 }
             }
         }
