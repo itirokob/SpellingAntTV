07af5832bf6daf8daada03c60b559f0de454471b
seongeunkim <grah.kim@gmail.com>
2018-05-25 16:15:03 -0300

    added page control

---
+++ SpellingBee/Controller/TutorialPageViewController.swift
@@ -0,0 +1,103 @@
+//
+//  TutorialPageViewController.swift
+//  SpellingBee
+//
+//  Created by Seong Eun Kim on 25/05/18.
+//  Copyright © 2018 Seong Eun Kim. All rights reserved.
+//
+
+import UIKit
+
+class TutorialPageViewController: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    
+    private var pageControl = UIPageControl()
+    
+
+    fileprivate lazy var pages: [UIViewController] = {
+        return [
+            self.getViewController(withIdentifier: "TutorialPage1"),
+            self.getViewController(withIdentifier: "TutorialPage2")
+        ]
+    }()
+    
+    fileprivate func getViewController(withIdentifier identifier: String) -> UIViewController
+    {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: identifier)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.dataSource = self
+        self.delegate = self
+        
+        if let firstVC = pages.first {
+            setViewControllers([firstVC], direction: .forward, animated: false, completion: nil)
+        }
+        
+        setupPageControl()
+    }
+
+    private func setupPageControl() {
+        
+        pageControl = UIPageControl(frame: CGRect(x: 0,y: UIScreen.main.bounds.maxY - 50,width: UIScreen.main.bounds.width,height: 50))
+        pageControl.tintColor = UIColor.black.withAlphaComponent(0)
+        pageControl.numberOfPages = pages.count
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
+        pageControl.currentPageIndicatorTintColor = UIColor.orange
+        pageControl.pageIndicatorTintColor = UIColor.lightGray.withAlphaComponent(0.8)
+
+        
+        let leading = NSLayoutConstraint(item: pageControl, attribute: .leading, relatedBy: .equal, toItem: view, attribute: .leading, multiplier: 1, constant: 0)
+        let trailing = NSLayoutConstraint(item: pageControl, attribute: .trailing, relatedBy: .equal, toItem: view, attribute: .trailing, multiplier: 1, constant: 0)
+        let bottom = NSLayoutConstraint(item: pageControl, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1, constant: 0)
+        
+        view.insertSubview(pageControl, at: 0)
+        view.bringSubview(toFront: pageControl)
+        view.addConstraints([leading, trailing, bottom])
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = pages.index(of: viewController) else { return nil }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0          else { return pages.last }
+        
+        guard pages.count > previousIndex else { return nil        }
+        
+        return pages[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = pages.index(of: viewController) else { return nil }
+        
+        let nextIndex = viewControllerIndex + 1
+        
+        guard nextIndex < pages.count else { return pages.first }
+        
+        guard pages.count > nextIndex else { return nil         }
+        
+        return pages[nextIndex]
+    }
+    
+    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return pages.count
+    }
+    
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return 0
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
